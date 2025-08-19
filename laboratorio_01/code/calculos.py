776723ef724aa9e481a7012a3b1aef9e208f9650
Lorrayne Marayze <lorrayne.marayze@gmail.com>
2025-08-18 21:39:18 -0300

    TODO: adição de função a ser definida

--- laboratorio_01/code/calculos.py
+++ laboratorio_01/code/calculos.py
@@ -7,3 +7,8 @@ def calcular_tempo_repositorio(created_at: datetime) -> float:
 def calcular_atualizacao_repositorio(updated_at: datetime) -> int:
     """Calcula e retorna quantos dias desde a última atualização do repositório."""
     return (datetime.now() - updated_at).days #updated_at: datetime da última atualização do repositório
+
+def calcular_razao_issues_fechadas(issues_fechadas: int, issues_total: int) -> float:
+    # TODO
+    
+    return issues_fechadas / issues_total
